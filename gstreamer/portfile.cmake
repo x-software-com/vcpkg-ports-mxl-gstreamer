6bbf62f5b256cae2a0b5a4a075cedaaacdb33c4a
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2026-01-22 08:13:57 +0100

    chore: update vcpkg to 2025.12.12, gstreamer to 1.26.9 and svt-avi to 3.1.2

--- gstreamer/portfile.cmake
+++ gstreamer/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_gitlab(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gstreamer/gstreamer
     REF "${VERSION}"
-    SHA512 2870d76fffd68bb5c702766a5dd3aa95f864c56a1d19444a3ad0a0e38fa1c66b25d61d5eb31046a3c53b875499ce7b784277cc9bf0bcbf698f35df0fb6a7b3f1
+    SHA512 c9079baf7e0d53dfd811e25a8070dacf0b8b582425144ab22e4df5c89af4533500f4828439b426a442c59a481e07904588a681e0cfb4e32911de32519db56db7
     HEAD_REF main
     PATCHES
         fix-mxl-live-recording-playback.patch
@@ -26,9 +26,10 @@ vcpkg_find_acquire_program(FLEX)
 vcpkg_find_acquire_program(BISON)
 vcpkg_find_acquire_program(NASM)
 
-# gstreamer/meson tends to pick host modules (e.g. libdrm),
-# so clean the search root unless explicitly set externally.
-if(VCPKG_CROSSCOMPILING AND "$ENV{PKG_CONFIG}$ENV{PKG_CONFIG_LIBDIR}" STREQUAL "")
+# gstreamer/meson tends to pick host modules (e.g. libdrm)
+# or X11 etc. from brew, so control installation order by
+# explicitly cleaning the search root unless set externally.
+if((VCPKG_CROSSCOMPILING OR VCPKG_TARGET_IS_OSX) AND "$ENV{PKG_CONFIG}$ENV{PKG_CONFIG_LIBDIR}" STREQUAL "")
     set(ENV{PKG_CONFIG_LIBDIR} "${CURRENT_INSTALLED_DIR}/share/pkgconfig")
 endif()
 
@@ -106,6 +107,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         fdkaac          gst-plugins-bad:fdkaac
         fluidsynth      gst-plugins-bad:fluidsynth
         gl              gst-plugins-bad:gl
+        hls             gst-plugins-bad:hls
         libde265        gst-plugins-bad:libde265
         microdns        gst-plugins-bad:microdns
         modplug         gst-plugins-bad:modplug
@@ -121,7 +123,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         srt             gst-plugins-bad:srt
         srtp            gst-plugins-bad:srtp
         svt-av1         gst-plugins-bad:svtav1
-        va              gst-plugins-bad:va   
+        va              gst-plugins-bad:va
         vulkan          gst-plugins-bad:vulkan
         wayland         gst-plugins-bad:wayland
         webp            gst-plugins-bad:webp
@@ -265,6 +267,7 @@ vcpkg_configure_meson(
         -Dgst-plugins-bad:gme=disabled
         -Dgst-plugins-bad:gs=disabled # Error during plugin configuration (abseil pkg-config file missing)
         -Dgst-plugins-bad:gsm=disabled
+        -Dgst-plugins-bad:hls-crypto=openssl
         -Dgst-plugins-bad:ipcpipeline=auto
         -Dgst-plugins-bad:iqa=disabled
         -Dgst-plugins-bad:kms=disabled
